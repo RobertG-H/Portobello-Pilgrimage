80f5e5d23b4dcbf1801ee9b870f2c8424786e618
RobertG-H <ultraconga@hotmail.com>
2018-05-27 12:52:13 -0400

    Added Yeti logo and made new scenes for main menu and level1

---
+++ Assets/Scripts/Yeti.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; // include so we can manipulate SceneManager
+
+public class Yeti : MonoBehaviour {
+
+	Animator anim;
+	AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		StartCoroutine (fade ());
+		audioSource = GetComponent<AudioSource>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	IEnumerator fade(){
+		yield return new WaitForSeconds(1.5f);
+		anim.SetTrigger ("fadeDone");
+		audioSource.Play();
+		yield return new WaitForSeconds(2.5f);
+		Application.LoadLevel("MainMenu");
+	}
+}
